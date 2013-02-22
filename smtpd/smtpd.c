7fd181b6fe3a1490fe894543d1492eed3e9973c2
Paul Fariello <paul@fariello.eu>
2013-02-22 23:56:50 +0100

    Add support for sasl auth

--- smtpd/smtpd.c
+++ smtpd/smtpd.c
@@ -53,6 +53,7 @@
 #include "smtpd.h"
 #include "log.h"
 #include "ssl.h"
+#include "sasl.h"
 
 static void parent_imsg(struct mproc *, struct imsg *);
 static void usage(void);
@@ -1562,7 +1563,7 @@ parent_auth_user(const char *username, const char *password)
 	(void)strlcpy(user, username, sizeof(user));
 	(void)strlcpy(pass, password, sizeof(pass));
 
-	ret = auth_userokay(user, NULL, "auth-smtp", pass);
+	ret = auth_sasl(user, pass);
 	if (ret)
 		return LKA_OK;
 	return LKA_PERMFAIL;
